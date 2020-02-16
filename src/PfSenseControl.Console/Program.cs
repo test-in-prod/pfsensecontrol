87bf9eb832cda56fa8a6e9f9b20461d22d60a9c5
crypton <crypton@crypton-technologies.net>
2020-02-16 00:12:03 -0800

    Testing

--- src/PfSenseControl.Console/Program.cs
+++ src/PfSenseControl.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace PfSenseControl.Console
 {
@@ -10,7 +11,7 @@ namespace PfSenseControl.Console
              * 0 - pfSense root URL ending with a slash
              * 1 - pfSense username
              * 2 - pfSense password
-             */ 
+             */
 
             string username = args[1];
             string password = args[2];
@@ -18,8 +19,14 @@ namespace PfSenseControl.Console
             using (var context = new PfSenseContext(args[0], new System.Net.NetworkCredential(username, password)))
             {
                 context.Login().GetAwaiter().GetResult();
-                var gateways = context.GetSystemGateways().GetAwaiter().GetResult();
-                gateways.SaveDefaultGateways(true).GetAwaiter().GetResult();
+
+                //var interfaces = context.GetStatusInterfacesAsync().GetAwaiter().GetResult();
+
+                //var gateways = context.GetSystemGatewaysAsync().GetAwaiter().GetResult();
+                //gateways.SaveDefaultGateways(true).GetAwaiter().GetResult();
+
+                var ovpnStatus = context.GetStatusOpenVPN().GetAwaiter().GetResult();
+
             }
         }
     }
